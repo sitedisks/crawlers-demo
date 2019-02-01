ef64f4ff9d9cc69d9f4df7c80d53396f70ab1edf
SiteDisks <peter@sitedisks.com.au>
2019-02-01 15:56:17 +1100

    Update spider2.py
    Guess we shall extract out the images for each link from the href_links list.

--- crawler-demo/spider2.py
+++ crawler-demo/spider2.py
@@ -10,13 +10,13 @@ all_images  = {} # website links as "keys" and images link as "values"
 response = requests.get('http://recurship.com/')
 selector = Selector(response.text)
 href_links = selector.xpath('//a/@href').getall()
-image_links = selector.xpath('//img/@src').getall()
 
 for link in href_links:
     try:
         response = requests.get(link)
         if response.status_code == 200:
-            image_links = selector.xpath('//img/@src').getall()
+            selector_each = Selector(response.text)
+            image_links = selector_each.xpath('//img/@src').getall()
             all_images[link] = image_links
     except Exception as exp:
         print('Error navigating to link : ', link)
